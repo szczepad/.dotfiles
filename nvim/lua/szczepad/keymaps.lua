8bff191dcfb4d7ec0e0d0727acba194d1d0df507
szczepad <info@dszczepanski.de>
2022-02-26 11:21:36 +0100

    feat(nvim): Added further Git-Plugins

--- nvim/lua/szczepad/keymaps.lua
+++ nvim/lua/szczepad/keymaps.lua
@@ -34,6 +34,9 @@ keymap("n","<S-h>", ":bprevious<CR>",opts)
 keymap("n","<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>",opts)
 keymap("n", "<c-f>", "<cmd>Telescope live_grep<cr>", opts)
 
+-- Git
+keymap("n","<leader>g", ":Git<CR>",opts)
+
 -- Visual --
 -- Indent
 keymap("v","<", "<gv", opts)
