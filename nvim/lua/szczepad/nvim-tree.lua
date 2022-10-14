09ee8fd9ba6e88155ea316934135dac321cf61c8
szczepad <dennis.szczepanski@web.de>
2022-10-14 20:49:16 +0200

    fixes null-ls and nvim-tree

--- nvim/lua/szczepad/nvim-tree.lua
+++ nvim/lua/szczepad/nvim-tree.lua
@@ -11,7 +11,6 @@ end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
-
 require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
   auto_reload_on_write = true,
   disable_netrw = false,
@@ -19,7 +18,7 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
   hijack_netrw = true,
   hijack_unnamed_buffer_when_opening = false,
   ignore_buffer_on_setup = false,
-  open_on_setup = false,
+  open_on_setup = true,
   open_on_setup_file = false,
   open_on_tab = false,
   sort_by = "name",
